f976b2b76f84aa0a052c750cd806db470ac28213
Himanshu Mehta <himanshumehta1114@gmail.com>
2020-03-28 14:21:11 +0530

    chore: remove dead code

--- src/content.js
+++ src/content.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./content.css";
+import App from './App';
 
-class Main extends React.Component {
-    render() {
-        return (
-            <div className={'my-extension'}>
-                <h1>Hello world - My first Extension</h1>
-            </div>
-        )
-    }
-}
-
-const app = document.createElement('div');
-app.id = "my-extension-root";
-document.body.appendChild(app);
-ReactDOM.render(<Main />, app);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
