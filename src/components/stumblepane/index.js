80f718a804cdcd523446d87362ee0e187abe0ff6
Himanshu Mehta <himanshumehta1114@gmail.com>
2020-04-03 11:27:52 +0530

    chore: implement chrome storage

--- src/components/stumblepane/index.js
+++ src/components/stumblepane/index.js
@@ -1,19 +1,28 @@
 /* global chrome */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./stumblepane.css";
 
 import Stumble from "./stumble";
 
 const Stumblepane = () => {
-  let stumbles = "";
+  let stumbles = [Array(3)];
 
   const [values, setValue] = useState(stumbles);
+
+  useEffect(() => {
+    chrome.storage.sync.get((res) => {
+      setValue(res.stumbles)
+    })
+  })
 
   const updateStumble = (val, index) => {
     let newVal = [...values];
 
     newVal[index] = val;
     setValue(newVal);
+    chrome.storage.sync.set({stumbles: newVal}, () => {
+      setValue(newVal)
+    })
   };
 
   return (
